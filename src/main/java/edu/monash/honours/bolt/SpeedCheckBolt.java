d7e34e0ead7821937ce8ffb2332cc50897f94a71
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2015-04-26 00:12:18 +1000

    Added basic processing logic for spout and bolt

--- src/main/java/edu/monash/honours/bolt/SpeedCheckBolt.java
+++ src/main/java/edu/monash/honours/bolt/SpeedCheckBolt.java
@@ -4,24 +4,75 @@ import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
 
 import java.util.Map;
 
 public class SpeedCheckBolt extends BaseRichBolt
 {
+  private static int SPEED_UPPER_BOUND = 90;
+  private static int SPEED_LOWER_LIMIT = 0;
+
+  private static int totalTupleCount = 0;
+  private int             tupleCount;
+  private OutputCollector collector;
+
+  /**
+   * Runs once when this bolt is created.
+   */
   public void prepare(final Map map, final TopologyContext topologyContext, final OutputCollector outputCollector)
   {
-
+    this.collector = outputCollector;
+    this.tupleCount = 0;
   }
 
+  /**
+   * Runs every time a tuple is received.
+   *
+   * @param tuple The input tuple containing a single speed field.
+   */
   public void execute(final Tuple tuple)
   {
+    updateTupleCounts();
 
+    double speed = tuple.getDouble(0);
+    Values emitTuple = checkSpeed(speed);
+
+    this.collector.emit(tuple, emitTuple);
+    this.collector.ack(tuple);
   }
 
+  /**
+   * Declare the output fields as being speed and a noise flag.
+   */
   public void declareOutputFields(final OutputFieldsDeclarer outputFieldsDeclarer)
   {
+    outputFieldsDeclarer.declare(new Fields("speed", "noiseFlag"));
+  }
 
+  /**
+   * Keeps count of tuples that pass through this bolt.
+   */
+  private void updateTupleCounts()
+  {
+    this.tupleCount++;
+    totalTupleCount += this.tupleCount;
+  }
+
+  /**
+   * Checks the given speed to see if it within limits.
+   *
+   * @param speed Speed to check.
+   * @return A Values tuple with two fields: speed and a noise flag. Noise flag is checked if speed exceeds limits.
+   */
+  private Values checkSpeed(double speed)
+  {
+    if (speed > SPEED_UPPER_BOUND || speed < SPEED_LOWER_LIMIT) {
+      return new Values(speed, true);
+    }
+
+    return new Values(speed, false);
   }
 }
--- src/main/java/edu/monash/honours/spout/SpeedOutputSpout.java
+++ src/main/java/edu/monash/honours/spout/SpeedOutputSpout.java
@@ -4,24 +4,55 @@ import backtype.storm.spout.SpoutOutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.util.Map;
 
 public class SpeedOutputSpout extends BaseRichSpout
 {
-  public void declareOutputFields(final OutputFieldsDeclarer outputFieldsDeclarer)
-  {
+  private SpoutOutputCollector collector;
+  private ServerSocket         serverSocket;
 
-  }
-
+  /**
+   * Runs once when this spout is created.
+   *
+   * @param map
+   * @param topologyContext
+   * @param spoutOutputCollector
+   */
   public void open(final Map map, final TopologyContext topologyContext,
                    final SpoutOutputCollector spoutOutputCollector)
   {
+    this.collector = spoutOutputCollector;
 
+    try {
+      this.serverSocket = new ServerSocket(9000);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
+  /**
+   * Run every time a new value is sent to this spout.
+   * <p>
+   * Emits a tuple containing a single speed value for further processing.
+   */
   public void nextTuple()
   {
+    double speed = 0;
 
+    Values emitTuple = new Values(speed);
+    this.collector.emit(emitTuple);
+  }
+
+  /**
+   * Declare the output field as being a single speed value.
+   */
+  public void declareOutputFields(final OutputFieldsDeclarer outputFieldsDeclarer)
+  {
+    outputFieldsDeclarer.declare(new Fields("speed"));
   }
 }
